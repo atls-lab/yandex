457326fa2b4ee385bbd8f38bd55acfc1d9271d4b
Egor Andrianov <tuogeniy@gmail.com>
2020-04-07 17:44:52 +0400

    feat(app): implement command for make fake purchases
    ISSUES CLOSED: #2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Command, flags } from '@oclif/command'
+import { FakeYandexAvisoRequest } from './FakeYandexAvisoRequest'
 
 class MakePurchaseCommand extends Command {
   public static description = 'describe the command here'
@@ -6,18 +7,38 @@ class MakePurchaseCommand extends Command {
   public static flags = {
     version: flags.version({ char: 'v' }),
     help: flags.help({ char: 'h' }),
-    secret: flags.string({ char: 's' }),
-    url: flags.string({ char: 'u', required: true }),
-    'purchase-id': flags.string({ char: 'p', required: true }),
-    'order-sum-amount': flags.integer({ char: 'o', required: true }),
+    secret: flags.string({
+      char: 's',
+      description: 'secret word to sign the request',
+      default: 'secret',
+    }),
+    url: flags.string({
+      char: 'u',
+      description: 'payment processor address',
+      default: 'https://payment-gateway.bs.local.aunited.dev/payment-aviso',
+    }),
+    purchaseId: flags.integer({
+      char: 'p',
+      description: 'purchase identifier',
+      required: true,
+    }),
+    orderSumAmount: flags.integer({
+      char: 'o',
+      description: 'payment amount',
+      required: true,
+    }),
   }
 
-  public static args = [{ name: 'file' }]
-
   public async run() {
-    const { args, flags } = this.parse(MakePurchaseCommand)
+    const parseOutput = this.parse(MakePurchaseCommand)
 
-    this.log('hello)0')
+    const { url, secret, purchaseId, orderSumAmount } = parseOutput.flags
+
+    const requestParams = { url, secret, purchaseId, orderSumAmount }
+
+    await new FakeYandexAvisoRequest(requestParams).execute()
+
+    this.log('💩 ОK!')
   }
 }
 
